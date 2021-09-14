b67a7635f1bfb0b75a40acb4291e703f3b3f8d4f
BearkillerPT <gilteixeira@ua.pt>
2021-09-14 19:07:52 +0100

    multiple client support!

--- Test.py
+++ Test.py
@@ -7,14 +7,13 @@ import math
 import cv2 as cv
 import numpy as np
 
-METINTEXTDISTANCE = 65
-healtbarrgbcolor = (102,51,51) #rgb
+METINTEXTDISTANCE = 60
 
 healthbarnotempty = False
 pickupkeypressed = False
 healthbar_located = False
 pixelcolor = (0, 0, 0)
-
+clients = []
 class Metin:
 
     def locateHealthBar():
@@ -37,20 +36,22 @@ class Metin:
                 print("Healthbarposition located: " + str(barlocation))
             
             return res
-    def locateMetinHealthBar():
-        metinhealthbarlocation = healthbarlocation = pyautogui.locateOnScreen('C:\\Users\\gil-t\\Downloads\\MetinBot-main\\images\\metin_hp.png', confidence=0.9, grayscale=True)
-        if healthbarlocation:
-            return metinhealthbarlocation
+    def locateAllandFilterMetinHealthBar(client):
+        metinhealthbarlocations = pyautogui.locateAllOnScreen('C:\\Users\\gil-t\\Downloads\\MetinBot-main\\images\\metin_hp.png', confidence=0.9, grayscale=True)
+        for healthbar in metinhealthbarlocations:
+            print(healthbar)
+            if healthbar[0] < client["healthbar"][0] + 700 and healthbar[0] + 800 > client["healthbar"][0] and healthbar[1] < client["healthbar"][1] and healthbar[1] > client["healthbar"][1] - 700:
+                return healthbar
 
 
-    def checkIfMetinStillAlive():
+
+    def checkIfMetinStillAlive(client):
         pyautogui.sleep(1)
         metinhealthbarlocation = 0
         template = cv.imread('C:\\Users\\gil-t\\Downloads\\MetinBot-main\\images\\metin_writing.png',0)
         w, h = template.shape[::-1]
         while not metinhealthbarlocation:
-            metinhealthbarlocation = Metin.locateMetinHealthBar()
-            print(metinhealthbarlocation)
+            metinhealthbarlocation = Metin.locateAllandFilterMetinHealthBar(client)
             screenshot = np.array(pyautogui.screenshot()) 
             img_gray = cv.cvtColor(screenshot, cv.COLOR_BGR2GRAY)
             if metinhealthbarlocation:
@@ -60,7 +61,6 @@ class Metin:
             return False
         leftouterpixellocation_x = int(metinhealthbarlocation.left -132)
         leftouterpixellocation_y = int(metinhealthbarlocation.top + h -5)
-        print("bar: " + str((leftouterpixellocation_x, leftouterpixellocation_y)))
         # Try to get the Pixel-Color
         checkedColor = False
         pixelcolor = (0, 0, 0)
@@ -82,21 +82,20 @@ class Metin:
 
     def collectLoot():
         print('collecting loot')
-        time.sleep(1)
         pydirectinput.press('z', 3, interval=0.1) #cause of us layout
 
 
-    def findMetinOpenCV():
-        metinhealthbarlocation = Metin.locateMetinHealthBar()
+    def findMetinOpenCV(client):
+        metinhealthbarlocation = Metin.locateAllandFilterMetinHealthBar(client)
         if metinhealthbarlocation:
             print("Bugged! Trying again!")
             pyautogui.press('esc')
             return False
-
-        screenshot = np.array(pyautogui.screenshot()) 
+        screenshot = np.array(pyautogui.screenshot())[ client["healthbar"][1] - 730 : client["healthbar"][1], client["healthbar"][0] - 10 :client["healthbar"][0] + 900]
         img_gray = cv.cvtColor(screenshot, cv.COLOR_BGR2GRAY)
         template = cv.imread('C:\\Users\\gil-t\\Downloads\\MetinBot-main\\images\\metin_writing.png',0)
         w, h = template.shape[::-1]
+        cv.imwrite('res.png',img_gray)
         res = cv.matchTemplate(img_gray,template,cv.TM_CCORR_NORMED)
         threshold = 0.85
         loc = np.where( res >= threshold)
@@ -108,10 +107,10 @@ class Metin:
         if not target:
             return False
         #for pt in zip(*loc[::-1]):
-        print(f'Metin found at {target[0] + w} and {target[1] + h + METINTEXTDISTANCE}')
-        pyautogui.moveTo(target[0] + int(w/2), target[1] + h + METINTEXTDISTANCE, 0.2)
+        print(f'Metin found at {target[0] + int(w/2)} and {target[1] + h + METINTEXTDISTANCE}')
+        pyautogui.moveTo(target[0] + int(w/2) + client["healthbar"][0] - 10, target[1] + h + METINTEXTDISTANCE + client["healthbar"][1] - 730, 0.2)
             #pyautogui.click(clicks=2, interval=0.1)
-        autoit.mouse_click("left",target[0] + int(w/2), target[1] + h + METINTEXTDISTANCE,2)
+        autoit.mouse_click("left",target[0] + int(w/2) + client["healthbar"][0] - 10, target[1] + h + METINTEXTDISTANCE + client["healthbar"][1] - 730,2)
             #cv.rectangle(img_gray, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
             #cv.imwrite('res.png',img_gray)
         return True
@@ -122,53 +121,67 @@ class Metin:
         print('looking around')
         pydirectinput.keyDown('numpad4')
         pydirectinput.keyDown('left')
-        time.sleep(1)
         pydirectinput.keyUp('numpad4')
         pydirectinput.keyUp('left')
+    def useSkills():
+        print('Using skills!')
+        pydirectinput.keyDown('ctrl')
+        pydirectinput.press('h')
+        pydirectinput.keyUp('ctrl')
+        time.sleep(1)
+        pydirectinput.press('3')
         time.sleep(2)
-
+        pydirectinput.press('4')
+        time.sleep(2)
+        pydirectinput.keyDown('ctrl')
+        pydirectinput.press('h')
+        pydirectinput.keyUp('ctrl')
     
 def run_bot():
     # Locate the Healthbar for init
-    healthbarlocation = 0
-    skills_interval = 0
-    while not healthbarlocation:
-        healthbarlocation = Metin.locateHealthBar()
-    while healthbarlocation:
-        if( time.time() - skills_interval > 300):
-            skills_interval = time.time()
-            print('Using skills!')
-            pydirectinput.keyDown('ctrl')
-            pydirectinput.press('h')
-            pydirectinput.keyUp('ctrl')
-            time.sleep(1)
-            pydirectinput.press('3')
-            time.sleep(2)
-            pydirectinput.press('4')
-            time.sleep(2)
-            pydirectinput.keyDown('ctrl')
-            pydirectinput.press('h')
-            pydirectinput.keyUp('ctrl')
-        #try to find a metin:
-        metinhealthbarlocation = Metin.locateMetinHealthBar()
-        if(metinhealthbarlocation):
-            pyautogui.press('esc')
-            Metin.lookaround()
-        if Metin.findMetinOpenCV():
-            start_time = time.time()
-             #check if the metin is still alive
-            while Metin.checkIfMetinStillAlive():
-                if(time.time() - start_time > 20):
-                    pyautogui.press('d')
-                    pyautogui.press('w')
+    healthbarlocations = 0
+    while not healthbarlocations:
+        healthbarlocations = Metin.locateHealthBar()
+    for location in healthbarlocations:
+        clients.append({"healthbar": location,
+                      "skills_timer" : 0,
+                      "bugged_timer": 0,
+                      "farming": False,
+                      "windowTop": (location[0], location[1] - 740)
+                     })
+    while True:
+        for client in clients:
+            pyautogui.moveTo(client["windowTop"][0], client["windowTop"][1], 0.2)
+            autoit.mouse_click("left",client["windowTop"][0], client["windowTop"][1],2)
+            
+            if( time.time() - client["skills_timer"] > 300):
+                client["skills_timer"] = time.time()
+                Metin.useSkills()
+            #try to find a metin:
+            #metinhealthbarlocation = Metin.locateMetinHealthBar()
+            #if(metinhealthbarlocation):
+            #    pyautogui.press('esc')
+            #    Metin.lookaround()
+            if not client["farming"]:
+                if Metin.findMetinOpenCV(client):
+                    client["bugged_timer"] = time.time()
+                    client["farming"] = True
+                
+                else:
+                    Metin.lookaround()
+                    Metin.lookaround()
                     break
-                time.sleep(1)
+                #check if the metin is still alive
+            if Metin.checkIfMetinStillAlive(client):
+                if(time.time() - client["bugged_timer"] > 20):
+                    pyautogui.press('d')
+                    pyautogui.press('d')
+            else:
+                client["farming"] = False
+                pyautogui.moveTo(client["windowTop"][0], client["windowTop"][1], 0.2)
+                autoit.mouse_click("left",client["windowTop"][0], client["windowTop"][1],2)
+                Metin.collectLoot()
 
-            Metin.collectLoot()
-
-        else:
-            time.sleep(2)
-            Metin.lookaround()
 
 
 if __name__ == '__main__':
