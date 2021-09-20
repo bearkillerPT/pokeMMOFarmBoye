c0e467c31816f5543d17229ff1c9e59d3b19d68e
BearkillerPT <gilteixeira@ua.pt>
2021-09-20 12:25:44 +0100

    small optimizations for desert farming

--- Test.py
+++ Test.py
@@ -23,7 +23,7 @@ trash_1 = 'C:\\Users\\gil-t\\Downloads\\MetinBot-main\\images\\items_to_delete\\
 trash_2 = 'C:\\Users\\gil-t\\Downloads\\MetinBot-main\\images\\items_to_delete\\leque_fenix.png'
 destroy_item = 'C:\\Users\\gil-t\\Downloads\\MetinBot-main\\images\\items_to_delete\\confirm_destroy.png'
 settings = 'C:\\Users\\gil-t\\Downloads\\MetinBot-main\\images\\settings.png'
-occupied_metin = 'C:\\Users\\gil-t\\Downloads\\MetinBot-main\\images\\occupied_metin.png'
+lvl = 'C:\\Users\\gil-t\\Downloads\\MetinBot-main\\images\\lvl.png'
 healthbarnotempty = False
 pickupkeypressed = False
 healthbar_located = False
@@ -57,7 +57,7 @@ class Metin:
         pyautogui.moveTo(client["window_top"][0] , client["window_top"][1] +200, 0.2)
         autoit.mouse_click("left",client["window_top"][0], client["window_top"][1]+200, 2)
         pydirectinput.press('f' + str(clients.index(client) + 2))
-        time.sleep(2)
+        time.sleep(3)
         pyautogui.moveTo(client["window_top"][0] , client["window_top"][1] , 0.2)
         autoit.mouse_click("left",client["window_top"][0], client["window_top"][1], 2)
         pyautogui.press('enter')
@@ -66,14 +66,18 @@ class Metin:
         pyautogui.sleep(2)
         pyautogui.keyUp('f')
 
-
-
-    def locateAllandFilterProp(client, prop_image, prop_type):
-        known_types = ["metin_health_bar" ,"logged_out", "deliver", "market", "orc_tooth", "submit", "trash", "destroy_item", "settings", "occupied_metin"]
-        prop_locations = pyautogui.locateAllOnScreen(prop_image, confidence=0.9, grayscale=True)
+    def locateAllAndFilterProp(client, prop_image):
+        res = []
+        prop_locations = pyautogui.locateAllOnScreen(prop_image, confidence=0.8, grayscale=True)
         for prop_location in prop_locations:
             if prop_location[0] > client["healthbar"][0] and prop_location[0] < client["healthbar"][0] + 800 and prop_location[1] < client["healthbar"][1] and prop_location[1] > client["healthbar"][1] - 900:
-                if known_types.__contains__(prop_type):
+                    res.append(prop_location)
+        return res
+
+    def locateAndFilterProp(client, prop_image):
+        prop_locations = pyautogui.locateAllOnScreen(prop_image, confidence=0.8, grayscale=True)
+        for prop_location in prop_locations:
+            if prop_location[0] > client["healthbar"][0] and prop_location[0] < client["healthbar"][0] + 800 and prop_location[1] < client["healthbar"][1] and prop_location[1] > client["healthbar"][1] - 900:
                     return prop_location
                 
 
@@ -82,7 +86,7 @@ class Metin:
         template = cv.imread('C:\\Users\\gil-t\\Downloads\\MetinBot-main\\images\\metin_writing.png',0)
         w, h = template.shape[::-1]
         while not metinhealthbarlocation:
-            metinhealthbarlocation = Metin.locateAllandFilterProp(client, metin_health_bar_image, "metin_health_bar")
+            metinhealthbarlocation = Metin.locateAndFilterProp(client, metin_health_bar_image)
             screenshot = np.array(pyautogui.screenshot()) 
             img_gray = cv.cvtColor(screenshot, cv.COLOR_BGR2GRAY)
             if metinhealthbarlocation:
@@ -115,11 +119,11 @@ class Metin:
         print('collecting loot')
         pyautogui.moveTo(client["window_top"][0] , client["window_top"][1] , 0.2)
         autoit.mouse_click("left",client["window_top"][0], client["window_top"][1], 2)
-        pydirectinput.press('z', 3, interval=0.1) #cause of us layout
+        pydirectinput.press('z') #cause of us layout
 
 
     def findMetinOpenCV(client):
-        metinhealthbarlocation = Metin.locateAllandFilterProp(client, metin_health_bar_image, "metin_health_bar")
+        metinhealthbarlocation = Metin.locateAndFilterProp(client, metin_health_bar_image)
         if metinhealthbarlocation:
             time.sleep(1)
             Metin.collectLoot(client)
@@ -139,22 +143,21 @@ class Metin:
         template = cv.imread('C:\\Users\\gil-t\\Downloads\\MetinBot-main\\images\\metin_writing.png',0)
         w, h = template.shape[::-1]
         res = cv.matchTemplate(img_gray,template,cv.TM_CCORR_NORMED)
-        threshold = 0.85
+        threshold = 0.9
         loc = np.where( res >= threshold)
         target_dist = 1000
         target = 0
+        yellow_pixels = 0
         for pt in zip(*loc[::-1]):
             red_pixels = 0
-            if pt[0] + w < len(screenshot) and pt[1] + h + 70 < len(screenshot[0]):
-                for i in range(pt[0], pt[0] + w):
-                    for j in range(pt[1] +70, pt[1] + h+ 70):
+            for i in range(pt[0], pt[0] + w):
+                for j in range(pt[1] + 55, pt[1] + h+ 70):
+                    if i < len(screenshot) and j < len(screenshot[0]):
                         pixel = screenshot[i, j]
                         if pixel[0] > 200 and pixel[1] < 50 and pixel[2] < 50:
-                            red_pixels += 1
-                #cv.rectangle(img_gray, (pt[0], pt[1] + 70), (pt[0] + w, pt[1] + h + 70), (255,0,0), 2)
-                #cv.imwrite('res.png',img_gray)         
-                if red_pixels < 10 and (math.sqrt(abs(300 - pt[0]) + abs(400- pt[1] + h + METINTEXTDISTANCE)) < target_dist and pt[0] + w < 750 and pt[1] + h + METINTEXTDISTANCE < 700):
-                    target = pt
+                            red_pixels += 1  
+            if red_pixels < 10 and (math.sqrt(pow(abs(300 - pt[0] + int(w/2)),2) +pow(abs(400- pt[1] + h + METINTEXTDISTANCE), 2)) < target_dist and pt[0] + w < 750 and pt[1] + h + METINTEXTDISTANCE < 700):
+                target = pt
         if not target:
             return False
         #for pt in zip(*loc[::-1]):
@@ -198,21 +201,22 @@ class Metin:
         print("biologist!")
         pyautogui.moveTo(client["window_top"][0] , client["window_top"][1] , 0.2)
         autoit.mouse_click("left",client["window_top"][0], client["window_top"][1], 2)
-        biologist_deliver_location = Metin.locateAllandFilterProp(client, biologist_deliver, "deliver")
+        biologist_deliver_location = Metin.locateAndFilterProp(client, biologist_deliver)
         if(biologist_deliver_location):
             pyautogui.moveTo(int((biologist_deliver_location[0] + biologist_deliver_location[0] + deliver_button_size)/2) , int((biologist_deliver_location[1] + biologist_deliver_location[1] + deliver_button_size)/2) , 0.2)
             autoit.mouse_click("left",int((biologist_deliver_location[0] + biologist_deliver_location[0] + deliver_button_size)/2), int((biologist_deliver_location[1] + biologist_deliver_location[1] + deliver_button_size)/2), 2)
             biologist_market_location = [0,0,10,10]
             pyautogui.sleep(1)
-            biologist_submit_location = Metin.locateAllandFilterProp(client, biologist_submit, "submit")
+            biologist_submit_location = Metin.locateAndFilterProp(client, biologist_submit)
             if biologist_submit_location:
                 biologist_market_location[0] = biologist_submit_location[0] + biologist_submit_location[2] + 42 
                 biologist_market_location[1] = biologist_submit_location[1] + 47
                 pyautogui.sleep(1)
                 pyautogui.moveTo(int((biologist_market_location[0] + biologist_market_location[0] + market_button_size)/2) , int((biologist_market_location[1] + biologist_market_location[1] + market_button_size)/2) , 0.2)
                 autoit.mouse_click("left",int((biologist_market_location[0] + biologist_market_location[0] + market_button_size)/2), int((biologist_market_location[1] + biologist_market_location[1] + market_button_size)/2), 2)
-                pyautogui.sleep(4)
-                biologist_orc_tooth_location = Metin.locateAllandFilterProp(client, biologist_orc_tooth, "orc_tooth")
+                pydirectinput.press('i')
+                pyautogui.sleep(1)
+                biologist_orc_tooth_location = Metin.locateAndFilterProp(client, biologist_orc_tooth)
                 if biologist_orc_tooth_location:
                     pyautogui.moveTo(int((biologist_orc_tooth_location[0] + biologist_orc_tooth_location[0] + orc_tooth_button_size)/2) , int((biologist_orc_tooth_location[1] + biologist_orc_tooth_location[1] + orc_tooth_button_size)/2) , 0.2)
                     autoit.mouse_click("right",int((biologist_orc_tooth_location[0] + biologist_orc_tooth_location[0] + orc_tooth_button_size)/2), int((biologist_orc_tooth_location[1] + biologist_orc_tooth_location[1] + orc_tooth_button_size)/2), 2)
@@ -239,14 +243,14 @@ class Metin:
         pydirectinput.press('i')
         trash_items = [trash_1, trash_2]
         for trash_item in trash_items:
-            trash = Metin.locateAllandFilterProp(client, trash_item, "trash")
+            trash = Metin.locateAndFilterProp(client, trash_item)
             while trash and not deleted.__contains__(trash) and time.time() - client["clear_inventory_bugged_timer"] < 50: 
                 pyautogui.moveTo(int((trash[0] + trash[0] + trash_size)/2) , int((trash[1] + trash[1] + trash_size)/2) , 0.1)
                 autoit.mouse_click("left",int((trash[0] + trash[0] + trash_size)/2), int((trash[1] + trash[1] + trash_size)/2), 1)
                 pyautogui.moveTo(client["window_top"][0] + 150, client["window_top"][1] + 150, 0.1)
                 autoit.mouse_click("left",client["window_top"][0] + 150, client["window_top"][1] + 150, 1)
                 pyautogui.sleep(0.1)
-                submit = Metin.locateAllandFilterProp(client, destroy_item, "destroy_item")
+                submit = Metin.locateAndFilterProp(client, destroy_item)
                 if submit:
                     print("Item Deleted!")
                     destroy_button = int(submit[0] + 3* submit[2]/4 + 20), int(submit[1] + submit[3]/2)
@@ -254,7 +258,7 @@ class Metin:
                     autoit.mouse_click("left", destroy_button[0], destroy_button[1], 1)
                     pyautogui.sleep(0.4)
                     deleted.append(trash)
-                    trash = Metin.locateAllandFilterProp(client, trash_item, "trash")
+                    trash = Metin.locateAndFilterProp(client, trash_item)
         pydirectinput.press('i')
         
 
@@ -262,7 +266,6 @@ class Metin:
             
     
 def run_bot():
-    
     bilogist = False
     # Locate the Healthbar for init
     healthbarlocations = 0
@@ -279,12 +282,13 @@ def run_bot():
                       "clear_inventory_bugged_timer" : 0,
                       "not_farming_loop_counter": 0,
                       "farming": False,
-                      "window_top": (location[0], location[1] - 740)
+                      "window_top": (location[0] + 10, location[1] - 740)
                      }
         clients.append(copy.deepcopy(append_dict))
         client_id += 1
     while True:
         for client in clients:
+            Metin.collectLoot(client)
             #noticed that sometimes it doesn't loot
             if client["not_farming_loop_counter"] > 5:
                 Metin.collectLoot(client)
@@ -298,12 +302,12 @@ def run_bot():
                 client["not_farming_loop_counter"] = 0
             if bilogist and (time.time() - client["biologist_timer"] > 650):
                 Metin.biologist(client)
-            loggout_location = Metin.locateAllandFilterProp(client, logged_out_image, "logged_out")
+            loggout_location = Metin.locateAndFilterProp(client, logged_out_image)
             if loggout_location:
                 Metin.handleLogout(clients, client) 
             pyautogui.moveTo(client["window_top"][0], client["window_top"][1], 0.2)
             autoit.mouse_click("left",client["window_top"][0], client["window_top"][1],2)
-            if Metin.locateAllandFilterProp(client, settings, "settings"):
+            if Metin.locateAndFilterProp(client, settings):
                 print("Closing Settings!")
                 pydirectinput.press('escape')
             if(time.time() - client["clear_inventory_timer"] > 500):
@@ -314,11 +318,16 @@ def run_bot():
                 client["skills_timer"] = time.time()
                 Metin.useSkills(client)
             #try to find a metin:
-            #metinhealthbarlocation = Metin.locateMetinHealthBar()
-            
+            if len(Metin.locateAllAndFilterProp(client, lvl)) > 1:
+                pydirectinput.keyDown('a')
+                pydirectinput.keyDown('s')
+                pyautogui.sleep(2)
+                pydirectinput.keyUp('a')
+                pydirectinput.keyUp('s')
+                break
             if not client["farming"]:
                 client["not_farming_loop_counter"] += 1
-                if(Metin.locateAllandFilterProp(client, metin_health_bar_image, "metin_health_bar")):
+                if(Metin.locateAndFilterProp(client, metin_health_bar_image)):
                     Metin.collectLoot(client)
                     pydirectinput.press('escape')
                     pydirectinput.keyDown('a')
@@ -327,7 +336,6 @@ def run_bot():
                     pydirectinput.keyUp('a')
                     pydirectinput.keyUp('s')
                     break
-                Metin.collectLoot(client)
                 if Metin.findMetinOpenCV(client):
                     client["bugged_timer"] = time.time()
                     client["farming"] = True
@@ -338,7 +346,7 @@ def run_bot():
                     break
                 #check if the metin is still alive
             if Metin.checkIfMetinStillAlive(client):
-                if(time.time() - client["bugged_timer"] > 30):
+                if(time.time() - client["bugged_timer"] > 20):
                     Metin.collectLoot(client)
                     pydirectinput.press('escape')
                     pydirectinput.keyDown('a')
@@ -348,14 +356,13 @@ def run_bot():
                     pydirectinput.keyUp('a')
                     pydirectinput.keyUp('s')
                     pydirectinput.keyUp('q')
+                    pydirectinput.keyDown('space')
+                    time.sleep(1)
+                    pydirectinput.keyUp('space')
                     print("UnBugging client " + str(client["client_id"]) + "!")
-                    client["farming"] = True
-                    client["bugged_timer"] = time.time()                    
                     break
             else:
                 client["farming"] = False
-                pyautogui.moveTo(client["window_top"][0], client["window_top"][1], 0.2)
-                autoit.mouse_click("left",client["window_top"][0], client["window_top"][1],2)
                 Metin.collectLoot(client)
                 if Metin.findMetinOpenCV(client):
                     client["bugged_timer"] = time.time()
