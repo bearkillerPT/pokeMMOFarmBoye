5358c8dc4c018b69847ce0fbe8fec2cc704d72af
BearkillerPT <gilteixeira@ua.pt>
2022-10-15 15:03:59 +0100

    updated readme and shiny detector working liike a charm

--- pokeGame.py
+++ pokeGame.py
@@ -3,11 +3,37 @@ import numpy as np
 import pyautogui
 import autoit
 import pydirectinput
+import pytesseract
+import pyscreenshot as sc
 
 class pokeGame:
     def __init__(self) -> None:
         pass
 
+    def check_for_shiny(self):
+        im = sc.grab(bbox=(500, 50, 1400, 210))
+        im.save('im.png')
+        img_cv = cv.imread('im.png')
+        img_rgb = cv.cvtColor(img_cv, cv.COLOR_BGR2GRAY)
+        hsv = cv.cvtColor(img_cv, cv.COLOR_BGR2HSV)
+
+        hsv_channels = cv.split(hsv)
+
+        rows = img_cv.shape[0]
+        cols = img_cv.shape[1]
+
+        for i in range(0, rows):
+            for j in range(0, cols):
+                h = hsv_channels[0][i][j]
+
+
+        txt = pytesseract.image_to_string( hsv_channels[0])
+        print(txt)
+        if 'Shiny' in txt or 'shiny' in txt or 'SHINY' in txt:
+            print('SHINY DETECTED')
+            return True
+        print('####\nNO SHINY DETECTED\n######')
+        return False
 
     def log_screenshot(self, ):
         screenshot = np.array(pyautogui.screenshot())
@@ -30,9 +56,9 @@ class pokeGame:
             pydirectinput.keyUp(key)
 
 
-    def detectImage(self, image):
+    def detectImage(self, image, confidence=0.6, grayscale=True):
         image_loc = pyautogui.locateAllOnScreen(
-            image, confidence=0.7, grayscale=True)
+            image, confidence=confidence, grayscale=grayscale)
         res = []
         if image_loc:
             for image_occ in image_loc:
