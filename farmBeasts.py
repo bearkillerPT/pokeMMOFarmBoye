28d3f0d560d1651037500218902c85c287ac3d27
bearkillerPT <gilteixeira@ua.pt>
2023-09-25 15:58:55 +0100

    added the horde farming script to the farmBeasts.py and added push notification this time with gotify!

--- farmBeasts.py
+++ farmBeasts.py
@@ -1,15 +1,16 @@
-# First go to a patch of grass 
-# walk around until you find a beast or another pokemon
-# Check if it contains a shiny
-# if it does, stop
-# if it doesn't, run away
-
 import time
 import pydirectinput
 import pyautogui
 import pytesseract
 import cv2 as cv
 import numpy as np
+import os
+import requests
+
+debug = False # if true, prints in useful functions
+sweetScentPP = 32 // 5 # 32 pp / 5 pokemon per sweet scent
+sweetScentKey = '7'
+teleportKey = '6'
 
 initialPos = 0
 # The names of pokemon are always in the top corner
@@ -33,15 +34,27 @@ def getScreenShotText():
     screenshot = pyautogui.screenshot(region=namesROI)
     screenshot = cv.cvtColor( np.array(screenshot), cv.COLOR_RGB2GRAY)
     # keep only white pixels
-    screenshot[screenshot < 250] = 0
+    screenshot[screenshot < 225] = 0
 
     # If you want to see the cropped screenshot where the pokemon name is supposed to be
     #screenshot.save('pokeNamesRegionOfInterest.png')
     text = pytesseract.image_to_string( screenshot).strip()
+    # every multiple of 2 is a pokemon name
+    #text = text.split('\n')
+    #if len(text) > 0:
+    #    text = ' '.join([text[i] for i in range(len(text)) if i % 2 == 0])  
+    #else:
+    #    text = ''
+    # join text with \n
     return text
 
 def isPokemon(text):
-    return text != '' and ('lv' in text.lower() or 'ly' in text.lower())
+    return text != '' and (
+        'lv.' in text.lower() or 
+        'lv,' in text.lower() or 
+        'ly.' in text.lower() or
+        'ly,' in text.lower()
+        )
 
 def isShiny(text):
     return 'shiny' in text.lower()    
@@ -49,26 +62,191 @@ def isShiny(text):
 def isBeast(text):
     return 'raikou' in text.lower() or 'entei' in text.lower() or 'suicune' in text.lower()
 
+def runAway():
+    pydirectinput.press('right')
+    time.sleep(.1)
+    pydirectinput.press('down')
+    time.sleep(.1)
+    pydirectinput.press('z')
 
-lastUICheck = time.time()
-while True:
-    text = getScreenShotText()
-    isTextPokemon = isPokemon(text)
-    if not text or not isTextPokemon:
-        walkAround(3)
-    else:
-        while (fight_ui := pyautogui.locateOnScreen(fight_ui_image, confidence=.7)) == None:
-            time.sleep(.5)
-        print("Found Pokemon: " + text)
-        if isShiny(text) or isBeast(text):
-            print('Found a shiny or beast! Exiting...')
+nightSlashPP = 15
+paydayPP = 20
+
+def useNightSlash():
+    global nightSlashPP
+    nightSlashPP -= 1
+    pydirectinput.press('z')
+    time.sleep(.1)
+    pydirectinput.press('down')
+    time.sleep(.1)
+    pydirectinput.press('right')
+    time.sleep(.1)
+    pydirectinput.press('z')
+
+def usePayday():
+    global paydayPP
+    paydayPP -= 1
+    pydirectinput.press('z')
+    time.sleep(.1)
+    pydirectinput.press('right')
+    time.sleep(.1)
+    pydirectinput.press('z')
+
+def farmBeast(): 
+    global debug
+    global encountersCount
+
+    while True:
+        if nightSlashPP == 0 or paydayPP == 0:
+            print('Out of PP! Exiting...')
             exit(0)
+        text = getScreenShotText()
+        isTextPokemon = isPokemon(text)
+        if debug:
+            print(text, end='\r')
+        if not text or not isTextPokemon:
+            walkAround(2)
         else:
-            pydirectinput.press('right')
-            time.sleep(.1)
-            pydirectinput.press('down')
-            time.sleep(.1)
-            pydirectinput.press('z')
-        while getScreenShotText() != '':
-            time.sleep(.5)
-        
+            print("Found Pokemon (try " + str(encountersCount) + "): " + text)
+            while (fight_ui := pyautogui.locateOnScreen(fight_ui_image, confidence=.7)) == None:
+                time.sleep(.5)
+            if isShiny(text) or isBeast(text):
+                print('Found a shiny or beast! Exiting...')
+                exit(0)
+            else:
+                with open('encountersCount.txt', 'w') as f:
+                    f.write(str(encountersCount + 1))
+                    encountersCount += 1
+                if 'diglett' in text.lower():
+                    usePayday()
+                    time.sleep(3)
+                    # check if the pokemon is still there
+                    # wait until the fight ui is there
+                    while (fight_ui := pyautogui.locateOnScreen(fight_ui_image, confidence=.7)) == None:
+                        time.sleep(.5)
+                        if not isPokemon(getScreenShotText()):
+                            break
+                    # use night slash to finish the fight
+                    if fight_ui != None:
+                        useNightSlash()
+                else:
+                    runAway()
+                
+                
+
+                while isPokemon(getScreenShotText()):
+                    time.sleep(.1)
+
+def goToPCAndHeal():
+    global debug
+
+    pydirectinput.press(teleportKey)
+    time.sleep(5)
+    if debug:
+        print('In front of the PC! Starting healing...')
+    pydirectinput.keyDown('z')
+    time.sleep(6)
+    pydirectinput.keyUp('z')
+    pydirectinput.keyDown('x')
+    pydirectinput.keyDown('z')
+    time.sleep(6)
+    pydirectinput.keyUp('z')
+    pydirectinput.keyUp('x')
+    if debug:
+        print('Exiting...')
+    pydirectinput.keyDown('down')
+    time.sleep(1.75)
+    pydirectinput.keyUp('down')
+    time.sleep(2)
+    print('Healed! Exiting...')
+
+
+def runJohtoFromPCToBellowSafari():
+    # cool place with hordes of tauras and
+    pydirectinput.keyDown('down')
+    time.sleep(.2)
+    pydirectinput.keyUp('down')
+    pydirectinput.keyDown('right')
+    time.sleep(1.2)
+    pydirectinput.keyUp('right')
+    pydirectinput.keyDown('down')
+    pydirectinput.keyDown('x')
+    time.sleep(4.75)
+    pydirectinput.keyUp('x')
+    pydirectinput.keyUp('down')
+
+def farmHordes():
+    global debug
+    global sweetScentPP
+    global encountersCount
+
+    print('Going to the horde location...')
+    runJohtoFromPCToBellowSafari()
+    print('Arrived! Starting to farm...')
+    for i in range(sweetScentPP): 
+        time.sleep(1)
+        while True:
+            text = getScreenShotText()
+            isTextPokemon = isPokemon(text)
+            if debug:
+                print(text, end='\r')
+            if not text or not isTextPokemon:
+                print('Using sweet scent... ' + str(i) + '/' + str(sweetScentPP), end='\r')
+                pydirectinput.press(sweetScentKey)
+                time.sleep(.5)
+            else:
+                print("Found Pokemon (try " + str(encountersCount) + " of Sweet Scent " + str(i) + "): " + text, end='\r')
+                while (fight_ui := pyautogui.locateOnScreen(fight_ui_image, confidence=.7)) == None:
+                    time.sleep(.5)
+                time.sleep(.5)
+                
+                if isShiny(text) or isBeast(text):
+                    # Replace with your Gotify server URL and application token
+                    gotify_url = 'http://localhost:80'
+                    app_token = 'ANhXu4_i1sel-NF'
+
+                    # Create a notification message
+                    message = {
+                        'title': 'It\'s a miracle!',
+                        'message': 'Found a shiny or beast! Exiting...',
+                        'priority': 7,
+                    }
+
+                    # Send the notification
+                    response = requests.post(f'{gotify_url}/message?token={app_token}', json=message)
+
+                    if response.status_code == 200:
+                        print('Notification sent successfully.')
+                    else:
+                        print(f'Failed to send notification. Status code: {response.status_code}')
+                        print(response.text)
+                    
+                    time.sleep(500)
+                    print('Found a shiny or beast! Exiting...')
+                    exit(0)
+                else:
+                    with open('encountersCount.txt', 'w') as f:
+                        f.write(str(encountersCount + 1))
+                        encountersCount += 1
+                        runAway()
+                newText = getScreenShotText()
+                while isPokemon((newText)):
+                    if debug:
+                        print("stuck", newText, isPokemon((newText)))
+                    time.sleep(.1)
+                    newText = getScreenShotText()
+                time.sleep(1)
+                break
+
+    goToPCAndHeal()
+
+
+# if a file with the encounters count exists, read it~
+encountersCount = 0
+if os.path.isfile('encountersCount.txt'):
+    with open('encountersCount.txt', 'r') as f:
+        encountersCount = int(f.readline())
+
+while True:
+    farmHordes()
+
